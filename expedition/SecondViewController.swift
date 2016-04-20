4ab8a15d9873bd0597cde7ba62fad1506eb8c085
Charly <j.gigonnet@gmail.com>
2016-04-20 10:49:26 +0200

    Initial Commit

---
+++ expedition/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  expedition
+//
+//  Created by J.C Gigonnet on 20/04/2016.
+//  Copyright © 2016 KLCT. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ expeditionTests/expeditionTests.swift
@@ -0,0 +1,36 @@
+//
+//  expeditionTests.swift
+//  expeditionTests
+//
+//  Created by J.C Gigonnet on 20/04/2016.
+//  Copyright © 2016 KLCT. All rights reserved.
+//
+
+import XCTest
+@testable import expedition
+
+class expeditionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
