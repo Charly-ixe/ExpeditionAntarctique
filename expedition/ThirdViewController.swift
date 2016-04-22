32b55f9976254f036acb44769bb9a9ebb032f511
Charly <j.gigonnet@gmail.com>
2016-04-22 11:20:56 +0200

    Add a third tab and make the app launch on the second one ▶️

---
+++ expedition/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  expedition
+//
+//  Created by J.C Gigonnet on 20/04/2016.
+//  Copyright © 2016 KLCT. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
