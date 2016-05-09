e034c82179d8d4951138a4d2e315901ac52f50b6
Charly <j.gigonnet@gmail.com>
2016-05-09 13:37:50 +0200

    Begin the UI on Données page 📶

--- expedition/ThirdViewController.swift
+++ expedition/ThirdViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ThirdViewController: UIViewController {
     
+    @IBOutlet weak var currentWeatherSubview: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
