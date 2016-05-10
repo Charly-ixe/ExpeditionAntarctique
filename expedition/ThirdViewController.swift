cf38ddb4e6e57c5f32ee749c5a68f79bf916c59d
Charly <j.gigonnet@gmail.com>
2016-05-10 10:46:45 +0200

    Add the ui view for current weather in Données Tab header 🌤

--- expedition/ThirdViewController.swift
+++ expedition/ThirdViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+let brashWhite = UIColor(red:36/255, green:90/255, blue:185/255, alpha:1.0)
+let nunatakBlack = UIColor(red:10/255, green:35/255, blue:80/255, alpha:1.0)
+let nunatakBlackAlpha = UIColor(red:10/255, green:35/255, blue:80/255, alpha:0.2)
+
 
 class ThirdViewController: UIViewController {
     
-    @IBOutlet weak var currentWeatherSubview: UIView!
+    // Outlets
+    @IBOutlet weak var currentWeatherView: UIView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+    @IBOutlet weak var windDirectionLabel: UILabel!
+    @IBOutlet weak var dataTemperatureLabel: UILabel!
+    @IBOutlet weak var celciusLabel: UILabel!
+    @IBOutlet weak var dataWindLabel: UILabel!
+    @IBOutlet weak var dataWindDirectionLabel: UILabel!
+    @IBOutlet weak var currentWeatherImageView: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Customize the currentWeatherView
+        
+        currentWeatherView.layer.cornerRadius = 6.0
+        currentWeatherView.layer.shadowColor = brashWhite.CGColor
+        currentWeatherView.layer.shadowOpacity = 0.2
+        currentWeatherView.layer.shadowOffset = CGSizeZero
+        currentWeatherView.layer.shadowRadius = 6.0
+        
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = UIBezierPath(roundedRect: currentWeatherImageView.bounds, byRoundingCorners: UIRectCorner.TopLeft.union(.BottomLeft), cornerRadii: CGSizeMake(6, 6)).CGPath
+        currentWeatherImageView.layer.mask = maskLayer
+        
+        temperatureLabel.textColor = nunatakBlackAlpha
+        windLabel.textColor = nunatakBlackAlpha
+        windDirectionLabel.textColor = nunatakBlackAlpha
+        dataTemperatureLabel.textColor = nunatakBlack
+        celciusLabel.textColor = nunatakBlack
+        dataWindLabel.textColor = nunatakBlack
+        dataWindDirectionLabel.textColor = nunatakBlack
+        
     }
     
     override func didReceiveMemoryWarning() {
