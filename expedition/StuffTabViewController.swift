6b3481cd46f89ed43fe9557a6bb38e1281914b5c
Charly <j.gigonnet@gmail.com>
2016-05-12 18:56:33 +0200

    Remake classes after a shitty commit 💩

--- expedition/StuffTabViewController.swift
+++ expedition/StuffTabViewController.swift
@@ -6,8 +6,18 @@
 //  Copyright © 2016 KLCT. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class StuffTabViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
--- expedition/WeatherTabViewController.swift
+++ expedition/WeatherTabViewController.swift
@@ -6,8 +6,22 @@
 //  Copyright © 2016 KLCT. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class WeatherTabViewController: UIViewController {
+    
+    @IBOutlet weak var weatherLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        weatherLabel.textColor = nunatakBlack
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
