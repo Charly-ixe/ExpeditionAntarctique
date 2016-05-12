02dafd4558f1072a765409c0c4a12b0999cd1e1f
Charly <j.gigonnet@gmail.com>
2016-05-12 18:34:38 +0200

    Make classes for the tabs controllers & continue the UI of the page 📒

---
+++ expedition/WeatherTabViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherTabViewController.swift
+//  expedition
+//
+//  Created by J.C Gigonnet on 12/05/2016.
+//  Copyright © 2016 KLCT. All rights reserved.
+//
+
+import Cocoa
+
+class WeatherTabViewController: UIViewController {
+
+}
